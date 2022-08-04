0417cec8888146e4ea73273e51a50c15d1358346
Paul Borgermans <paul.borgermans@kuleuven.be>
2022-08-04 23:11:48 +0200

    added utility function for metadata schema flattening

--- src/lib/util.py
+++ src/lib/util.py
@@ -26,3 +26,39 @@ def strip_suffix(_string: str, _suffix: str):
     if _string.endswith(_suffix):
         _string = _string[: -len(_suffix)]
     return _string
+
+def flatten_josse_schema(object_tuple, level=0, prefix="", result_dict={}):
+    """
+    """
+    (_key, _dict) = object_tuple
+    for p_key, _property in _dict["properties"].items():
+        # Preprocessing/sanitizing
+        # Checkboxes type
+        if p_key.endswith("_checkboxes"):
+            # chop off the type from the label
+            p_key = p_key[: -len("_checkboxes")]
+            # put the type where it belongs
+            _property["type"] = "checkboxes"
+        # select or radio buttons: presence of enum list an its size
+        if "enum" in _property:
+            _property["type"] = "select" if len(_property["enum"]) >= 5 else "radio"
+        # Real processing: either get a new (possible composite field) or walk down a child object
+
+        if _property["type"] == "object":
+            result_dict = flatten_josse_schema(
+                (p_key, _property),
+                level=(level + 1),
+                prefix=f"{prefix}.{p_key}",
+                result_dict=result_dict,
+            )
+
+        else:
+
+            result_dict[f"{prefix}.{p_key}"] = {
+                "label": _property["title"],
+                "type": _property["type"],
+            }
+
+    # print(f"level: {level}")
+    # pprint(result_dict)
+    return result_dict
