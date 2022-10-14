7cba97d27c56afe5d607ce8d882230337e34ce6c
Paul Borgermans <paul.borgermans@kuleuven.be>
2022-10-14 15:47:31 +0200

    Changing ports for local podman build/runs

--- podman.sh
+++ podman.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-podman build -t mango:1.0 Docker/Dockerfile
-podman run --rm -it --name mango-slim -p 8887:80 mango:1.0
+podman build -t mango:1.0 .
+podman run --rm -it --name mango -p 8080:8080 mango:1.0
