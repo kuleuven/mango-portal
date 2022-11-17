de13181ae962f28ea78e19eb02af7a4dd4495c2c
Paul Borgermans <paul.borgermans@kuleuven.be>
2022-11-17 11:11:54 +0100

    Expose a dedicated server string instead of python-irodsclient

--- src/run_dev.sh
+++ src/run_dev.sh
@@ -8,4 +8,5 @@ echo "Starting development instance from directory $(dirname $0)"
 export FLASK_APP=app.py
 # development mode for hot reloading changed files
 export FLASK_DEBUG=True
+export spOption="Mango_portal_dev"
 flask run
