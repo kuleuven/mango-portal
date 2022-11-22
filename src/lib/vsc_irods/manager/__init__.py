8eddb183a0727410b6821d380dc3d07759ff3872
Paul Borgermans <paul.borgermans@kuleuven.be>
2022-11-22 21:54:09 +0100

    removed unused vsc_irods module + smaller tweaks

--- src/lib/vsc_irods/manager/__init__.py
+++
@@ -1,8 +0,0 @@
-class Manager:
-    """ Base class for 'managers' that are attached to a VSCiRODSSession """
-
-    def __init__(self, session):
-        self.session = session
-
-    def log(self, *args, **kwargs):
-        self.session.log(*args, **kwargs)
