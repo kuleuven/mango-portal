266da1827e187aa29e4da25ec70bfc46c3c6506b
Paul Borgermans <paul.borgermans@kuleuven.be>
2022-12-16 17:26:56 +0100

    Fixed merge induced problem: import signals module restored

--- src/common/browse.py
+++ src/common/browse.py
@@ -47,6 +47,8 @@ from operator import itemgetter
 browse_bp = Blueprint("browse_bp", __name__, template_folder="templates/common")
 
 from metadata_schema.editor import get_metadata_schema_dir
+import signals
+
 
 def group_prefix_metadata_items(
     metadata_items, mango_prefix, no_schema_label="other", group_analysis_unit=False,
