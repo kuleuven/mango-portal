88838f54ba4e3577a7562988218992c5890a9f01
Peter Verraedt <peter.verraedt@kuleuven.be>
2023-01-31 17:08:06 +0100

    Show connection information
    Signed-off-by: Peter Verraedt <peter.verraedt@kuleuven.be>
    
    Fix typo
    
    Signed-off-by: Peter Verraedt <peter.verraedt@kuleuven.be>
    
    Add <code>
    
    Signed-off-by: Peter Verraedt <peter.verraedt@kuleuven.be>
    
    Update instructions to accomodate pre 4.3.0 clients
    
    Signed-off-by: Peter Verraedt <peter.verraedt@kuleuven.be>

--- src/plugins/data_platform/__init__.py
+++ src/plugins/data_platform/__init__.py
@@ -1,6 +1,10 @@
 import os
+import logging
 
 API_URL = os.environ.get(
     "API_URL", "https://icts-p-coz-data-platform-api.cloud.icts.kuleuven.be"
 )
 API_TOKEN = os.environ.get("API_TOKEN", "")
+
+if not API_TOKEN:
+    logging.warn(f"No COZ API token, module data_platform will not work")
