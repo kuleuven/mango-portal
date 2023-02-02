2d8248c3ec8d064a3b86ce2681f7578df86fdb51
Peter Verraedt <peter.verraedt@kuleuven.be>
2023-02-02 20:13:38 +0100

    Membership modals
    Signed-off-by: Peter Verraedt <peter.verraedt@kuleuven.be>

--- src/plugins/data_platform/project.py
+++ src/plugins/data_platform/project.py
@@ -2,7 +2,9 @@ import requests
 from flask import (
     Blueprint,
     render_template,
-    session,
+    redirect,
+    request,
+    url_for,
 )
 
 from . import API_URL, current_user_api_token
@@ -34,3 +36,15 @@ def project(project_name):
     return render_template(
         "project/project_view.html.j2", project=project, zones=zones,
     )
+
+@data_platform_project_bp.route("/data-platform/projects/member/add", methods=["POST"])
+def add_project_member():
+    return redirect(url_for('data_platform_project_bp.project', project_name=request.form.get('project')))
+
+@data_platform_project_bp.route("/data-platform/projects/member/modify", methods=["POST"])
+def modify_project_member():
+    return redirect(url_for('data_platform_project_bp.project', project_name=request.form.get('project')))
+
+@data_platform_project_bp.route("/data-platform/projects/member/delete", methods=["POST"])
+def delete_project_member():
+    return redirect(url_for('data_platform_project_bp.project', project_name=request.form.get('project')))
