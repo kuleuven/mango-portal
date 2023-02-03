bfb3e8673a606a75d862fedf522da45db9a68d95
Peter Verraedt <peter.verraedt@kuleuven.be>
2023-02-03 19:46:39 +0100

    Show link to project folders
    Signed-off-by: Peter Verraedt <peter.verraedt@kuleuven.be>

--- src/plugins/data_platform/project.py
+++ src/plugins/data_platform/project.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from flask import (
     Blueprint,
     render_template,
@@ -97,8 +98,23 @@ def deploy_project():
     return redirect(url_for('data_platform_project_bp.project', project_name=id))
 
 
-@data_platform_project_bp.route("/data-platform/project/<project_name>/api_token/<type>", methods=["GET"])
+@data_platform_project_bp.route("/data-platform/project/<project_name>/api_token/<type>", methods=["GET", "POST"])
 def api_token(project_name, type):
+    if request.method == 'GET':
+        return render_template(
+            "project/api_token.html.j2", project_name=project_name, type=type,
+        )
+
+    header = {"Authorization": "Bearer " + current_user_api_token()}
+    response = requests.post(
+        f"{API_URL}/v1/irods/projects/{project_name}/machine_token", headers=header, json={"type": type},
+    )
+    response.raise_for_status()
+
+    info = response.json()
+
     return render_template(
-        "project/api_token.html.j2", project_name=project_name,
+        "project/api_token_connection_info.html.j2", project_name=project_name, type=type,
+        info=info,
+        setup_json=json.dumps(info['irods_environment'], indent=4),
     )
