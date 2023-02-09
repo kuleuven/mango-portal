390c602b5ba95d6af89c696e8ec2bc11660cafba
Peter Verraedt <peter@verraedt.be>
2023-02-09 17:14:22 +0100

    Comment line in run_waitress.sh
    Signed-off-by: Peter Verraedt <peter@verraedt.be>

--- src/run_waitress.sh
+++ src/run_waitress.sh
@@ -16,7 +16,7 @@ export OIDC_CLIENT_ID=oidcapp
 
 # To test kuleuven login locally, uncomment the following lines and ask Peter for the secret
 # See also https://ceifdoc.icts.kuleuven.be/devops/development/go-webapp-basis/#basis-setup-development
-export OPENID_REDIRECT_BASE=http://oidcapp.icts.kuleuven.be:3000
+#export OPENID_REDIRECT_BASE=http://oidcapp.icts.kuleuven.be:3000
 #export OIDC_SECRET=
 
 export API_URL=https://icts-q-coz-data-platform-api.cloud.q.icts.kuleuven.be
