88d504c4022f42e268528fee8a272b70fb88776a
Mariana <mariana.montes@kuleuven.be>
2023-03-17 09:22:01 +0100

    add lazy loading to bulk operations

--- src/static/dist/bulk_operations.js
+++ src/static/dist/bulk_operations.js
@@ -55,94 +55,65 @@ function apply_bulk_operation() {
             ? n_collections_printed
             : `${n_collections_printed} and ${n_dobjects_printed}`;
 
-    const previous_toasts = [...document.querySelectorAll('div.toast')]
-        .filter((div) => div.id.startsWith(selected_option.value));
-    const toast_id = `${selected_option.value}-items-${previous_toasts.length}`;
+    const confirmation_modal = document.querySelector('div.modal#confirmation-dialog');
+    const modal = bootstrap.Modal.getOrCreateInstance(confirmation_modal);
+    const confirmation_form = confirmation_modal.querySelector('form#confirmation-form');
+    confirmation_form.querySelector('input#action').value = selected_option.value;
     
-    const form_data = new FormData();
-    form_data.append('action', selected_option.value);
     const items_to_send = selected_option.value == 'copy'
         ? selected_items.filter((c) => c.startsWith('dobj-'))
-        : selected_items;                            
-    items_to_send.forEach((item) => form_data.append('items', item));
-    
-                            
+        : selected_items;
+    const confirmation_text = confirmation_form.querySelector('p#confirmation-text')
+    // clean previous items if existing
+    const existing_items = confirmation_form.querySelectorAll('input[name="items"]');
+    if (existing_items.length > 0) {
+        existing_items.forEach((x) => x.remove());
+    }
+    items_to_send.forEach((item) => {
+        const new_input = document.createElement('input');
+        new_input.type = 'hidden';
+        new_input.name = 'items';
+        new_input.value = item;
+        confirmation_form.querySelector('.modal-body').insertBefore(new_input, confirmation_text);
+    });
+    const del_checkbox = confirmation_form.querySelector('div.form-check#force-delete');
+
     if (selected_option.value == 'copy' || selected_option.value == 'move') {
-        const offcanvas_dom = document.querySelector('.offcanvas#collection-tree');
-        offcanvas_dom.querySelectorAll('i.bi-folder-symlink-fill').forEach((i) => i.remove());
-        const offcanvas = new bootstrap.Offcanvas(offcanvas_dom);
-        offcanvas_dom.querySelectorAll('input[type="radio"]')
-            .forEach((input) => input.addEventListener('change', () => {
-                offcanvas_dom.querySelectorAll('i.bi-folder-symlink-fill')
-                    .forEach((i) => i.remove());
-                // parent_folders == 0 if the destination folder is not the direct parent of a selected folder
-                parent_folders = selected_items.filter((c) => c.startsWith('col-' + input.value)).length;
-                // child_folders == 0 if the destination folder is not a child of a selected folder
-                child_folders = selected_items.filter((c) => {
-                    return c.startsWith('col-') && input.value.startsWith(c.match('(?<prefix>col-)(?<path>.+)').groups.path)
-                }).length;
-                if (parent_folders + child_folders == 0) {
-                    const icon = document.createElement('i');
-                    icon.className = 'bi bi-folder-symlink-fill ms-2';
-                    icon.setAttribute('style', 'font-size:1.2rem;');
-                    input.nextSibling.appendChild(icon);
-                    icon.addEventListener('click', (e) => {
-                        e.preventDefault();
-                        const action = selected_option.value == 'copy' ? 'copied' : 'moved';
-                        const destination = input.value;
-                        toast_message = selected_option.value == 'copy' && n_collections > 0
-                            ? `${n_dobjects_printed} will be ${action} to "${destination}. Copying collections is not supported."`
-                            : `${n_items} will be ${action} to "${destination}."`
-                        const toast = new Toast(toast_id, toast_message);
-                        toast.show(() => {
-                            form_data.append('destination', destination);
-                            
-                            const xhr = new XMLHttpRequest();
-                            xhr.open('POST', urls.getAttribute('post'), true);
-                            xhr.send(form_data);
-
-                            offcanvas.toggle();
-                            select_all.checked = false;
-                            tbody_checkboxes.forEach((c) => c.checked = false);
-                            go_button.setAttribute('disabled', '');
-                            select_all.parentElement.removeChild(badge_counter);
-                            location.reload();
-                        });
-                    });
-                }
-
-            }));
+        offcanvas.offcanvas.querySelectorAll('i.bi-folder-symlink-fill').forEach((i) => i.remove());
+        if (del_checkbox != undefined) {
+            del_checkbox.remove();
+        }
+        const action = this.selected_option == 'copy' ? 'copied' : 'moved';
+        toast_message = this.selected_option == 'copy' && n_collections > 0
+            ? `${n_dobjects_printed} will be ${action} to "DESTINATION". Copying collections is not supported.`
+            : `${n_items} will be ${action} to "DESTINATION."`
+                    
         offcanvas.toggle();
-        offcanvas_dom.querySelectorAll('.collapse.show').forEach((collapsible) => {
-            bootstrap.Collapse.getOrCreateInstance(collapsible).toggle();
-        });
+        offcanvas.link_data(selected_items, confirmation_text, confirmation_form, modal, selected_option.value, toast_message);
+        offcanvas.update();
 
     } else {
-        const toast = new Toast(toast_id, `${n_items} will be deleted.`);
-        const toast_body = toast.div.querySelector('div.toast-body');
-        const force_checkbox = Field.quick('div', 'form-check');
-        const checkbox_input = Field.quick('input', 'form-check-input');
-        checkbox_input.type = 'checkbox';
-        checkbox_input.id = 'force-checkbox';
-        force_checkbox.appendChild(checkbox_input);
-        checkbox_label = Field.quick('label', 'form-check-label', 'Delete permanently');
-        checkbox_label.setAttribute('for', 'force-checkbox');
-        force_checkbox.appendChild(checkbox_label);
 
-        toast_body.insertBefore(force_checkbox, toast_body.querySelector('div.row'));
-        toast.show(() => {
-            form_data.set('action', checkbox_input.checked ? 'force_delete' : 'delete');
-            
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', urls.getAttribute('post'), true);
-            xhr.send(form_data);
+        const del_checkbox = confirmation_form.querySelector('div.form-check#force-delete');
+        if (del_checkbox == undefined) {
+            confirmation_form.querySelector('input#destination').value = "";
+            confirmation_modal.querySelector('p#confirmation-text').innerHTML = `${n_items} will be deleted.`;
+            const del_checkbox = Field.quick('div', 'form-check');
+            del_checkbox.id = 'force-checkbox';
+            const checkbox_input = Field.quick('input', 'form-check-input');
+            checkbox_input.type = 'checkbox';
+            checkbox_input.name = 'force_delete';
+            checkbox_input.value = true;
+            del_checkbox.appendChild(checkbox_input);
+            checkbox_label = Field.quick('label', 'form-check-label', 'Delete permanently');
+            checkbox_label.setAttribute('for', 'force-checkbox');
+            del_checkbox.appendChild(checkbox_label);
+            confirmation_form.insertBefore(del_checkbox, confirmation_text);
+        } else {
+            del_checkbox.removeAttribute('checked');
+        }
+        modal.show()
 
-            select_all.checked = false;
-            tbody_checkboxes.forEach((c) => c.checked = false);
-            go_button.setAttribute('disabled', '');
-            select_all.parentElement.removeChild(badge_counter);
-            location.reload();
-        });
     }
 }
 
@@ -156,6 +127,11 @@ class OffCanvas {
         this.add_header();
         this.add_body();
 
+    }
+
+    toggle() {
+        this.bootstrap = new bootstrap.Offcanvas(this.offcanvas);
+        this.bootstrap.toggle();
     }
 
     add_header() {
@@ -176,26 +152,96 @@ class OffCanvas {
 
     add_body() {
         this.body = Field.quick('form', 'offcanvas-body offcanvas-tree');
-
         this.offcanvas.appendChild(this.body);
     }
 
     append_to(container) {
         container.appendChild(this.offcanvas);
     }
+    link_data(selected_items, confirmation_text, confirmation_form, modal, selected_option, message) {
+        this.selected_items = selected_items;
+        this.confirmation_text = confirmation_text;
+        this.confirmation_form = confirmation_form;
+        this.modal = modal;
+        this.selected_option = selected_option;
+        this.message = message;
+    }
+
+    update_radios() {
+        this.offcanvas.querySelectorAll('input[type="radio"]')
+        .forEach((input) => input.addEventListener('change', () => {
+            console.log(input)
+            this.offcanvas.querySelectorAll('i.bi-folder-symlink-fill')
+                .forEach((i) => i.remove());
+            // parent_folders == 0 if the destination folder is not the direct parent of a selected folder
+            let parent_folders = this.selected_items.filter((c) => c.startsWith('col-' + input.value)).length;
+            // child_folders == 0 if the destination folder is not a child of a selected folder
+            let child_folders = this.selected_items.filter((c) => {
+                return c.startsWith('col-') && input.value.startsWith(c.match('(?<prefix>col-)(?<path>.+)').groups.path)
+            }).length;
+            if (parent_folders + child_folders == 0) {
+                const icon = document.createElement('i');
+                icon.className = 'bi bi-folder-symlink-fill ms-2';
+                icon.setAttribute('style', 'font-size:1.2rem;');
+                input.nextSibling.appendChild(icon);
+                icon.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    const destination = input.value;
+                    this.confirmation_text.innerHTML = this.message.replace('DESTINATION', destination);
+                    this.confirmation_form.querySelector('input#destination').value = destination;
+                    this.modal.show();
+                });
+            }
+
+        }));
+    }
+
+    update() {
+        this.offcanvas.addEventListener('shown.bs.offcanvas', () => {
+            if (this.offcanvas.querySelectorAll('input[type="radio"]').length == 0) {
+                top_tree.fill();
+            }
+            this.update_radios();
+            
+            this.offcanvas.querySelectorAll('.collapse.show').forEach((collapsible) => {
+                bootstrap.Collapse.getOrCreateInstance(collapsible).toggle();
+            });
+        });
+        if (this.offcanvas.classList.contains('show')) {
+            this.update_radios();
+        }
+    }
 }
 
 class TreeElement {
-    constructor({ id, label, children = undefined } = {}) {
-        this.has_children = children != undefined;
+    constructor(button_group, path = '') {
+        this.url = urls.getAttribute('tree') + path;
+        this.button_group = button_group;
+        this.spinner = button_group.querySelector('div#spinner-div');
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', () => this.build_tree(xhr.responseText));
+        xhr.open('GET', this.url, false);
+        this.xhr = xhr;
+    }
+
+    fill() {
+        this.xhr.send();
+    }
+
+    build_tree(text) {
+        const { path, name, children } = JSON.parse(text);
+        this.has_children = children.length > 0;
         if (this.has_children) {
             this.children = children;
         }
-        this.id = id.replaceAll('/', '-');
-        this.value = id;
-        this.label_name = label;
+        this.path = path;
+        this.id = path.replaceAll('/', '-');
+        this.value = path;
+        this.name = name;
         this.input = this.create_input();
         this.label = this.create_label();
+        this.spinner.remove();
+        this.append_to(this.button_group);
     }
 
     append_to(parent_element) {
@@ -228,7 +274,7 @@ class TreeElement {
         const label = Field.quick('label', 'btn btn-oc text-start');
         label.setAttribute('for', this.id + '-radio');
         const collection_name = document.createElement('span');
-        collection_name.innerHTML = this.label_name;
+        collection_name.innerHTML = this.name;
         label.appendChild(collection_name);
 
         if (this.has_children) {
@@ -243,10 +289,16 @@ class TreeElement {
         if (!this.has_children) {
             return;
         }
+        let children_requested = false;
 
         const collapse = Field.quick('div', 'collapse ps-3 w-100');
         collapse.id = this.id + '-collapse';
         const icon = this.label.querySelector('i.bi');
+        
+        const sub_button_group = TreeElement.create_button_group();
+        collapse.appendChild(sub_button_group);
+
+        this.branches = this.children.map((tree_element) => new TreeElement(sub_button_group, tree_element.path));
 
         // the events below have an if statement because otherwise the icons of
         // the parent folders also change
@@ -254,6 +306,14 @@ class TreeElement {
             if (collapse.classList.contains('show')) {
                 icon.classList.replace('bi-caret-right-fill', 'bi-caret-down-fill');
             }
+            if (!children_requested) {
+                this.branches.forEach((tree_element) => {
+                    tree_element.fill();
+                });
+            }
+            offcanvas.update();
+            children_requested = true;
+            
         });
 
         collapse.addEventListener('hidden.bs.collapse', () => {
@@ -262,20 +322,30 @@ class TreeElement {
             }
         });
 
-        const sub_button_group = TreeElement.create_button_group();
-        collapse.appendChild(sub_button_group);
-
-        this.children.forEach((tree_element) => {
-            new TreeElement(tree_element).append_to(sub_button_group);
-        });
-
+        
         return collapse;
+    }
+
+    static create_spinner() {
+        let spinner_div = Field.quick('div', 'd-flex justify-content-center');
+        spinner_div.id = 'spinner-div';
+        
+        let spinner = Field.quick('div', 'spinner-border spinner-border-sm text-primary');
+        spinner.role = 'status';
+        spinner_div.appendChild(spinner);
+        
+        let spinner_span = Field.quick('span', 'text-muted ms-3', 'Loading tree...');
+        spinner_div.appendChild(spinner_span);
+
+        return spinner_div;
     }
 
     static create_button_group() {
         const button_group = Field.quick('div', 'btn-group-vertical w-100');
         button_group.role = 'group';
         button_group.setAttribute('aria-label', 'Collection tree to select destination');
+        
+        button_group.appendChild(TreeElement.create_spinner());
         return button_group;
     }
 }
@@ -285,14 +355,5 @@ const button_group = TreeElement.create_button_group();
 offcanvas.body.appendChild(button_group);
 offcanvas.append_to(document.getElementById('collection-content'));
 
-var xhr = new XMLHttpRequest();
-function create_tree() {
-    const tree = JSON.parse(this.responseText);
-    tree.forEach((tree_element) => {
-        new TreeElement(tree_element).append_to(button_group);
-    });
-}
-xhr.addEventListener('load', create_tree);
-xhr.open("GET", urls.getAttribute('tree'));
-xhr.send();
+const top_tree = new TreeElement(button_group);
 
