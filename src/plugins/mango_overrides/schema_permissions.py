8138aa44f76b83fa4e5fd5636e48c267a487664a
Paul Borgermans <paul.borgermans@kuleuven.be>
2023-08-08 22:46:46 +0200

    Add also the realm manager besides the schema manager group who have full rights

--- src/plugins/mango_overrides/schema_permissions.py
+++ src/plugins/mango_overrides/schema_permissions.py
@@ -6,10 +6,12 @@ from irods.session import iRODSSession
 
 SCHEMA_MANAGER_GROUP_SUFFIX = "schema_manager"
 
+
 class GroupBasedSchemaPermissions(BaseSchemaPermissionsManager):
     def __init__(self, zone: str, realm: str):
         super().__init__(zone, realm)
         self.group_schema_manager = f"{realm}_{SCHEMA_MANAGER_GROUP_SUFFIX}"
+        self.realm_manager = f"{realm}_manager"
 
     def get_user_permissions_realm(self, irods_session: iRODSSession):
         # check if the realm is the user personal space and grant full access
@@ -22,16 +24,19 @@ class GroupBasedSchemaPermissions(BaseSchemaPermissionsManager):
             if "mango_portal_admin" in irods_session.roles:
                 return self.allow_all
             else:
-                return self.schema_permissions['read']
+                return self.schema_permissions["read"]
         # realm schema managers "{realm}_schema_manager" have full rights
         # realm members only read rights
-        if self.group_schema_manager in irods_session.my_group_names:
+        if any(
+            group in irods_session.my_group_names
+            for group in (self.group_schema_manager, self.realm_manager)
+        ):
             return self.allow_all
         if self.realm in irods_session.my_group_names:
-            return self.schema_permissions['read']
-        
+            return self.schema_permissions["read"]
+
         # if no matches, nothing allowed
         return self.deny_all
-    
+
     def get_user_permissions_schema(self, irods_session: iRODSSession, schema):
         return self.inherit_permissions
