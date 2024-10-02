876b8d594469691f3f0b8e6c92284f1b8c3a558e
Paul Borgermans <paul.borgermans@kuleuven.be>
2024-10-02 23:51:51 +0200

    trying to solve redirect from https to http

--- src/kernel/common/browse.py
+++ src/kernel/common/browse.py
@@ -819,7 +819,9 @@ def delete_data_object():
     return redirect(request.referrer)
 
 
-@browse_bp.route("/collection/upload/stream/<path:collection>", methods=["POST", "PUT"])
+
+
+@browse_bp.route("/collection/upload/stream/<collection>", methods=["POST", "PUT"])
 @csrf.exempt
 def collection_upload_stream(collection: str):
     collection = unquote(collection)
