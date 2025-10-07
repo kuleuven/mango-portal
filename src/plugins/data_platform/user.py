ebf2af5521e69093adcea1a4252271fc8dc80ea6
Peter Verraedt <peter@verraedt.be>
2025-10-07 15:36:01 +0200

    Pass new paramaters to /data-platform/connection-info too
    Signed-off-by: Peter Verraedt <peter@verraedt.be>

--- src/plugins/data_platform/user.py
+++ src/plugins/data_platform/user.py
@@ -297,6 +297,7 @@ def connection_info():
     setup_json={
         'linux': json.dumps(info['irods_environment'], indent=4),
         'windows': json.dumps({**info['irods_environment'], 'irods_authentication_uid': 1000}, indent=4),
+        'linux_pam_interactive': json.dumps({**info['irods_environment'], 'irods_authentication_scheme': "pam_interactive"}, indent=4),
     }
 
     if "-hpc-" in jobid:
@@ -308,13 +309,21 @@ def connection_info():
         if parts[1] != 'p':
             info['hpc-irods-setup-zone'] += "-" + parts[1]
 
+    accounttype = "kuleuven"
+
+    if info['irods_environment']['irods_user_name'].startswith("vsc"):
+        accounttype = "vsc"
+
     sftp_host = "rdmsftp.icts.kuleuven.be"
+    desktop_sync = f"icts-p-coz-desktop-sync-reva-{accounttype}.cloud.icts.kuleuven.be"
     if "-q-" in jobid:
         sftp_host = "rdmsftp.q.icts.kuleuven.be"
+        desktop_sync = "icts-q-coz-desktop-sync-reva-{accounttype}.cloud.q.icts.kuleuven.be"
     if "-t-" in jobid:
-        sftp_host = "rdmsftp.t.icts.kuleuven.be"    
+        sftp_host = "rdmsftp.t.icts.kuleuven.be"
+        desktop_sync = "icts-t-coz-desktop-sync-reva-{accounttype}.cloud.t.icts.kuleuven.be"
 
-    return render_template("user/connection_info.html.j2", info=info, jobid=jobid, setup_json=setup_json, sftp_host=sftp_host)
+    return render_template("user/connection_info.html.j2", info=info, jobid=jobid, setup_json=setup_json, sftp_host=sftp_host, desktop_sync=desktop_sync)
 
 
 @data_platform_user_bp.route('/data-platform/retrieve-token', methods=["GET", "POST"])
